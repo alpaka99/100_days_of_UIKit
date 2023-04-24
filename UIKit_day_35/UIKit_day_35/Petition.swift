5ad4f284083bb7572be86ac22556e39907a29fa4
Lets <ruvan123@naver.com>
2023-04-25 01:14:39 +0900

    [230425] UIKit_day_35

---
+++ UIKit_day_35/UIKit_day_35/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  UIKit_day_35
+//
+//  Created by user on 2023/04/25.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ UIKit_day_35/UIKit_day_35/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  UIKit_day_35
+//
+//  Created by user on 2023/04/25.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
