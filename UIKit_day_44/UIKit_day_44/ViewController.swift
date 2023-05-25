c7ae0cb9992ed9cd8a46554996a2f6da1f131f86
Lets <ruvan123@naver.com>
2023-05-25 21:08:55 +0900

    [230525] UIKit_day_44

--- UIKit_day_44/UIKit_day_44/ViewController.swift
+++ UIKit_day_44/UIKit_day_44/ViewController.swift
@@ -1,19 +1,18 @@
 //
 //  ViewController.swift
-//  UIKit_day_43
+//  UIKit_day_44
 //
-//  Created by user on 2023/05/18.
+//  Created by user on 2023/05/25.
 //
 
 import UIKit
 
 class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     var people = [Person]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
     }
 
@@ -23,12 +22,12 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
-            fatalError("Cannot convert dequeReusableCell to PersonCell")
+            fatalError("Cannot type cast dequeReusableCell to Person cell")
         }
         
         let person = people[indexPath.item]
         
-        cell.labelView.text = person.name
+        cell.name.text = person.name
         
         let path = getDocumentsDirectory().appendingPathComponent(person.image)
         cell.imageView.image = UIImage(contentsOfFile: path.path)
@@ -37,16 +36,28 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         cell.imageView.layer.borderWidth = 2
         cell.imageView.layer.cornerRadius = 3
         cell.layer.cornerRadius = 7
-        cell.layer.borderColor = UIColor(ciColor: .black).cgColor
-        cell.layer.borderWidth = 2
         
         return cell
     }
     
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let ac = UIAlertController(title: "Change Name", message: "To what name?", preferredStyle: .alert)
+        
+        ac.addTextField()
+        
+        ac.addAction(UIAlertAction(title: "Change", style: .default) { [weak self] _ in
+            self?.people[indexPath.item].name = (ac.textFields?[0].text)!
+            collectionView.reloadData()
+        })
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        present(ac, animated: true)
+    }
+    
     @objc func addNewPerson() {
         let picker = UIImagePickerController()
-        picker.allowsEditing = true // 사용자가 사진을 자를 수 있도록 해주는
-        picker.delegate = self //
+        picker.allowsEditing = true
+        picker.delegate = self
         present(picker, animated: true)
     }
     
@@ -54,43 +65,23 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         guard let image = info[.editedImage] as? UIImage else { return }
         
         let imageName = UUID().uuidString
-        
         let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
         
         if let jpegData = image.jpegData(compressionQuality: 0.8) {
             try? jpegData.write(to: imagePath)
         }
         
-        let person = Person(name: "Unknown", image: imageName)
+        let person = Person(image: imageName, name: "Unknown")
         people.append(person)
-        
         collectionView.reloadData()
         
         dismiss(animated: true)
     }
     
+    
     func getDocumentsDirectory() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return paths[0]
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return path[0]
     }
-    
-    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let person = people[indexPath.item]
-        
-        let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
-        ac.addTextField()
-        
-        ac.addAction(UIAlertAction(title: "ok", style: .default) { [weak self, weak ac] _ in
-            guard let newName = ac?.textFields?[0].text else { return }
-            person.name = newName
-            self?.collectionView.reloadData()
-            }
-        )
-        
-        ac.addAction(UIAlertAction(title: "Cancel", style: .default))
-        
-        present(ac, animated: true)
-    }
-    
-    
 }
+
