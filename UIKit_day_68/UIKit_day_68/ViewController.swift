8920922120c5f73188a75628eddd76151f65d065
Lets <ruvan123@naver.com>
2023-08-13 21:25:09 +0900

    [230813] UIKit_day_68

---
+++ UIKit_day_68/UIKit_day_68/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit_day_68
+//
+//  Created by user on 2023/08/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
