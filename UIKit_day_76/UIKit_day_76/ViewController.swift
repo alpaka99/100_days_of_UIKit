4e877416eb7627684e0b41ef040c06eb99ed5944
Lets <ruvan123@naver.com>
2023-09-01 16:55:40 +0900

    [230831] UIKit_day_76

---
+++ UIKit_day_76/UIKit_day_76/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit_day_76
+//
+//  Created by user on 2023/09/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
