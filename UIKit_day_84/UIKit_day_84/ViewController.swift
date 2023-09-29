6617e89b98b485888c5067cfa4cf9fb4622683fe
Lets <ruvan123@naver.com>
2023-09-29 17:00:42 +0900

    [230929] UIKit_day_84

--- UIKit_day_84/UIKit_day_84/ViewController.swift
+++ UIKit_day_84/UIKit_day_84/ViewController.swift
@@ -1,20 +1,24 @@
 //
 //  ViewController.swift
-//  UIKit_day_83
+//  UIKit_day_84
 //
-//  Created by user on 2023/09/27.
+//  Created by user on 2023/09/29.
 //
+
 import LocalAuthentication
 import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var secret: UITextView!
+    let password = "Open Sesame"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         title = "Nothing to see here"
+        
+        KeychainWrapper.standard.set(password, forKey: "Password")
         
         let notificationCenter = NotificationCenter.default
         notificationCenter.addObserver(self, selector: #selector(adjustKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
@@ -34,23 +38,20 @@ class ViewController: UIViewController {
         } else {
             secret.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
         }
-        
         secret.scrollIndicatorInsets = secret.contentInset
         
         let selectedRange = secret.selectedRange
         secret.scrollRangeToVisible(selectedRange)
-           
     }
-    
+
     func unlockSecretMessage() {
         secret.isHidden = false
-        title = "Secret Stuff!"
+        title = "Secret stuff!"
         
-        if let text = KeychainWrapper.standard.string(forKey: "SecretMessage") {
-            secret.text = text
-        }
+        secret.text = KeychainWrapper.standard.string(forKey: "SecretMessage") ?? ""
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Lock", image: UIImage(systemName: "lock"), target: self, action: #selector(saveSecretMessage))
     }
-    
     
     @objc func saveSecretMessage() {
         guard secret.isHidden == false else { return }
@@ -58,39 +59,54 @@ class ViewController: UIViewController {
         KeychainWrapper.standard.set(secret.text, forKey: "SecretMessage")
         secret.resignFirstResponder()
         secret.isHidden = true
+        navigationItem.rightBarButtonItem?.isHidden = true
         title = "Nothing to see here"
-        
     }
     
+    
     @IBAction func authenticationTapped(_ sender: Any) {
-//        unlockSecretMessage()
+        
         let context = LAContext()
         var error: NSError?
-        let array: Array<Int> = .init()
         
         if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-            let reason = "Identify yourself!" // Touch ID request message
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { [weak self] success, authenticationError in
+            
+            let reason = "Identify yourself!"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { [weak self] success, error in
+                
                 DispatchQueue.main.async {
                     if success {
                         self?.unlockSecretMessage()
                     } else {
                         // error
-                        let ac = UIAlertController(title: "Authentication Failed", message: "Please try again", preferredStyle: .alert)
-                        ac.addAction(UIAlertAction(title: "Cancel", style: .default))
+                        let ac = UIAlertController(title: "Error!", message: "Face ID Failed", preferredStyle: .alert)
+                        
+                        ac.addTextField { (textField) in
+                            textField.placeholder = "Enter Password"
+                        }
+                        
+                        ac.addAction(UIAlertAction(title: "OK", style: .default) { _ in
+                            guard let input = ac.textFields?[0].text else { return }
+                            guard let password = KeychainWrapper.standard.string(forKey: "Password") else { return }
+                            
+                            if input == password {
+                                self?.unlockSecretMessage()
+                            }
+                            
+                        })
+                        
+                        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
                         
                         self?.present(ac, animated: true)
                     }
                 }
             }
         } else {
-            // no biometric
-            let ac = UIAlertController(title: "Biometry unavailable", message: "Your device is not configured for biometry authentication", preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "Cancel", style: .default))
-            
-            present(ac, animated: true)
+            // no biometry
+            print("No biometry")
         }
     }
-    
 }
+
 
