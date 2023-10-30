204fb88cfd37a15c03dcbdca6804980aca4eaca7
Lets <ruvan123@naver.com>
2023-10-31 01:06:20 +0900

    [231031] UIKit_day_96

---
+++ UIKit_day_96/UIKit_day_91/MyGenreViewController.swift
@@ -0,0 +1,174 @@
+//
+//  MyGenreViewController.swift
+//  UIKit_day_91
+//
+//  Created by user on 2023/10/30.
+//
+
+import UIKit
+import CloudKit
+
+class MyGenreViewController: UITableViewController {
+    var myGenres: [String]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let defaults = UserDefaults.standard
+        if let savedGenres = defaults.object(forKey: "myGenres") as? [String] {
+            myGenres = savedGenres
+        } else {
+            myGenres = [String]()
+        }
+
+        
+        title = "Notify me about..."
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(saveTapped))
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    @objc func saveTapped() {
+        let defaults = UserDefaults.standard
+        defaults.set(myGenres, forKey: "myGenres")
+        
+        let database = CloudStore.store.container.publicCloudDatabase
+        
+        database.fetchAllSubscriptions { [unowned self] (subscriptions, error) in
+            if error == nil {
+                if let subscriptions = subscriptions {
+                    for subscription in subscriptions {
+                        database.delete(withSubscriptionID: subscription.subscriptionID) { str, error in
+                            if error != nil {
+                                print(error?.localizedDescription)
+                            }
+                        }
+                    }
+                    
+                    for genre in self.myGenres {
+                        let predicate = NSPredicate(format: "genre = %@", genre)
+                        let subscription = CKQuerySubscription(recordType: "Whistles", predicate: predicate)
+                        
+                        let notification = CKSubscription.NotificationInfo()
+                        notification.alertBody = "There's a new whistle in the \(genre)"
+                        notification.soundName = "default"
+                        
+                        subscription.notificationInfo = notification
+                        
+                        database.save(subscription) { result, error in
+                            if let error = error {
+                                print(error.localizedDescription)
+                            }
+                        }
+                    }
+                }
+            } else {
+                print(error?.localizedDescription)
+            }
+        }
+        
+        navigationController?.popToRootViewController(animated: true)
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return SelectGenreController.genres.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let genre = SelectGenreController.genres[indexPath.row]
+        cell.textLabel?.text = genre
+        
+        if myGenres.contains(genre) {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            let selectedGenre = SelectGenreController.genres[indexPath.row]
+            
+            if cell.accessoryType == .none {
+                cell.accessoryType = .checkmark
+                myGenres.append(selectedGenre)
+            } else {
+                cell.accessoryType = .none
+                if let index =  myGenres.firstIndex(of: selectedGenre) {
+                    myGenres.remove(at: index)
+                }
+            }
+        }
+    }
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ UIKit_day_96/UIKit_day_91/ViewController.swift
@@ -0,0 +1,130 @@
+//
+//  ViewController.swift
+//  UIKit_day_91
+//
+//  Created by user on 2023/10/20.
+//
+
+import CloudKit
+import UIKit
+
+class ViewController: UITableViewController {
+    static var isDirty = true
+    var whistles = [Whistle]()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let indexPath = tableView.indexPathForSelectedRow {
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
+        
+        if ViewController.isDirty {
+            loadWhistle()
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        title = "What's that Whistle?"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addWhistle))
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "Home", style: .plain, target: nil, action: nil)
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Genres", style: .plain, target: self, action: #selector(selectGenre))
+    }
+
+    
+    @objc func addWhistle() {
+        let vc = RecordWhistleViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+
+    func loadWhistle() {
+        // part 1
+        let pred = NSPredicate(value: true)
+        let sort = NSSortDescriptor(key: "creationDate", ascending: false)
+        let query = CKQuery(recordType: "Whistles", predicate: pred)
+        query.sortDescriptors = [sort]
+        
+        let operation = CKQueryOperation(query: query)
+        operation.desiredKeys = ["genre", "comments"]
+        operation.resultsLimit = 50
+        
+        var newWhistles = [Whistle]()
+        
+        // part 2
+        operation.recordMatchedBlock = {(recordID, result) in
+            let whistle = Whistle()
+            whistle.recordID = recordID
+            let record = try? result.get()
+            whistle.genre = record?["genre"]
+            whistle.comments = record?["comments"]
+            newWhistles.append(whistle)
+        }
+        
+        
+        // part 3
+        operation.queryResultBlock = { [unowned self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .failure(let error):
+                    let ac = UIAlertController(title: "Fetch Failed ", message: "\(error.localizedDescription)", preferredStyle: .alert)
+                    ac.addAction(UIAlertAction(title: "OK", style: .default))
+                    present(ac, animated: true)
+                case .success(let ckQueryCursor):
+                    ViewController.isDirty = false
+                    self.whistles = newWhistles
+                    self.tableView.reloadData()
+                }
+            }
+        }
+        
+        // part 4
+//        CloudStore.store.container.add(operation)
+        CloudStore.store.container.publicCloudDatabase.add(operation)
+    }
+    
+    func makeAttributedString(title: String, subtitle: String) -> NSAttributedString {
+        let titleAttributes = [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .headline), NSAttributedString.Key.foregroundColor: UIColor.purple]
+        let subtitleAttributes = [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .subheadline)]
+        
+        let titleString = NSMutableAttributedString(string: "\(title)", attributes: titleAttributes)
+        
+        if subtitle.count > 0 {
+            let subtitleString = NSAttributedString(string: "\n\(subtitle)", attributes: subtitleAttributes)
+            titleString.append(subtitleString)
+        }
+        
+        return titleString
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.accessoryType = .disclosureIndicator
+        cell.textLabel?.attributedText = makeAttributedString(title: whistles[indexPath.row].genre, subtitle: whistles[indexPath.row].comments)
+        cell.textLabel?.numberOfLines = 0
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return whistles.count
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = ResultViewController()
+        vc.whistle = whistles[indexPath.row]
+        
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @objc func selectGenre() {
+        let vc = MyGenreViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
